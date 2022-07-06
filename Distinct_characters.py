d77d6266efb62facb41a20d8a1eecfb4583cccb1
Pallavi <97425707+Pallavi-kemburu@users.noreply.github.com>
2022-07-06 15:43:56 +0530

    s=input() s=s.lower() k="" for i in s:     if s.count(i)==1 and i!=' ':         k+=i k=sorted(k) l=''.join(k) print(l)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,15 +1,9 @@
-a=input()
-arr=[]
-for i in a:
-    if i.isspace():
-        continue
-    else:
-        if i.lower() not in arr:
-            arr.append(i.lower())
-for i in range(len(arr)):
-    for j in range(len(arr)-1):
-        if arr[j]>arr[j+1]:
-            arr[j],arr[j+1]=arr[j+1],arr[j]
-for i in arr:
-    print(i,end="")
-
+s=input()
+s=s.lower()
+k=""
+for i in s:
+    if s.count(i)==1 and i!=' ':
+        k+=i
+k=sorted(k)
+l=''.join(k)
+print(l)

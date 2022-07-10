bd372c4be81b57386da5f2e0d06b87686ff1705d
Pallavi <97425707+Pallavi-kemburu@users.noreply.github.com>
2022-07-10 08:18:51 +0530

    n=int(input()) a=list(map(int,input().split())) i=0 b=[] while i!=n//2:     b.append(a[i])     b.append(a[(n-1)-i])     i=i+1 if n%2!=0:     b.append(a[i])     b.append(0) print(*b)

---
+++ First_and_Last_pair.py
@@ -0,0 +1,13 @@
+n=int(input())
+a=list(map(int,input().split()))
+i=0
+b=[]
+while i!=n//2:
+    b.append(a[i])
+    b.append(a[(n-1)-i])
+    i=i+1
+if n%2!=0:
+    b.append(a[i])
+    b.append(0)
+print(*b)
+        

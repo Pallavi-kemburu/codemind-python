16d3fc73c9f73787d45401b0d48118fa805078bb
Pallavi <97425707+Pallavi-kemburu@users.noreply.github.com>
2022-07-12 15:22:20 +0530

    s=input() s=s.split() c=0 v='aeiouAEIOU' for i in s:     if i[0] in v and i[-1] not in v:         c=c+1 print(c)

--- count_words.py
+++ count_words.py
@@ -1,7 +1,8 @@
 s=input()
 s=s.split()
 c=0
+v='aeiouAEIOU'
 for i in s:
-    if i[0] in "aeiouAEIOU" and i[len(i)-1] not in 'aeiouAEIOU':
-        c+=1
+    if i[0] in v and i[-1] not in v:
+        c=c+1
 print(c)
